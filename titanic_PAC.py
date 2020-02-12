8bbdaa1a299e0624d86a029481c5045aad5577b8
jkim3289 <60450463+jkim3289@users.noreply.github.com>
2020-02-11 22:37:02 -0500

    Update titanic_PAC.py

--- titanic_PAC.py
+++ titanic_PAC.py
@@ -2,13 +2,14 @@
 import pandas as pd
 # Numpy lets us work with arrays
 import numpy as np
-import re
 import matplotlib.pyplot as plt
+import re
 # Sklearn provides various modules with a common API
 from sklearn import svm, tree, neighbors, neural_network
 from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
+
 
 
 
@@ -137,6 +138,7 @@ X_train = train_data.loc[:]
 
 X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.33, random_state=10)
 
+
 #End of processing
 
 from sklearn.linear_model import PassiveAggressiveClassifier
@@ -147,7 +149,132 @@ PAC_clf.fit(X_train.values, y_train.values)
 print(PAC_clf.score(X_test.values, y_test.values))
 y_pred = PAC_clf.predict(X_test.values)
 y_truth = y_test.values
+
+tn, fp, fn, tp = confusion_matrix(y_truth, y_pred).ravel()
+print("\nConfusion Matrix")
+print(confusion_matrix(y_truth, y_pred, labels=[0, 1]))
+print("")
+print("True Negatives", tn)
+print("False Positives", fp)
+print("False Negatives", fn)
+print("True Positives", tp)
+
+##################################
+## confusion matrix
+
+import itertools
+import matplotlib.pyplot as plt
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+
+
+# Compute confusion matrix
+cnf_matrix = confusion_matrix(y_truth, y_pred)
+class_names=['0', '1']
+np.set_printoptions(precision=2)
+
+# Plot non-normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=class_names,
+                      title='Confusion matrix, without normalization')
+
+plt.show()
+#######################################
 FP = len([x for x in range(len(y_pred)) if y_truth[x] == False and y_pred[x] == True])
 FN = len([x for x in range(len(y_pred)) if y_truth[x] == True and y_pred[x] == False])
-print("FP: " + str(FP / len(y_truth)))
+print("\nFP: " + str(FP / len(y_truth)))
 print("FN: " + str(FN / len(y_truth)))
+
+
+
+#  Fairly fast for many datapoints, less fast for many dimensions
+def find_pareto(data):
+    is_Pareto = np.ones(data.shape[0], dtype = bool)
+    for i, c in enumerate(data):
+        # Keep any point with a lower cost
+        if is_Pareto[i]:
+            # This is where you would change for miniminzation versus maximization 
+
+            # Minimization
+            is_Pareto[is_Pareto] = np.any(data[is_Pareto]<c, axis=1)  
+
+            # Maximization
+            #is_Pareto[is_Pareto] = np.any(data[is_Pareto]>c, axis=1)  
+
+            # And keep self
+            is_Pareto[i] = True  
+
+    # Downsample from boolean array
+    Pareto_data = data[is_Pareto, :]
+
+    # Sort data
+    Pareto_out =  Pareto_data[np.argsort(Pareto_data[:,0])]
+
+    #return is_Pareto
+    return Pareto_out
+
+# Create random list of values
+N=20
+myData = np.random.random((N,2))
+
+# Include the trival Pareto points, i.e. always on or off
+myData = np.vstack(([[0,1],[1,0]], myData))
+
+# Use above routine to find pareto points
+myPareto=find_pareto(myData)
+
+# Calculate the Area under the Curve as a Riemann sum
+auc = np.sum(np.diff(myPareto[:,0])*myPareto[0:-1,1])
+
+# Create figure
+plt.figure()
+
+# Make sure font sizes are large enough to read in the presentation
+plt.rcParams.update({'font.size': 14})
+
+# Plot all points
+plt.scatter(myData[:,0],myData[:,1],)
+
+# Plot Pareto steps. note 'post' for minimization 'pre' for maximization
+plt.step(myPareto[:,0], myPareto[:,1], where='post')
+#plt.step(myPareto[:,0], myPareto[:,1], where='pre')
+
+# Make sure you include labels
+# Minimization
+plt.title('Example of a Minimization Result\n with AUC = ' + str(auc))
+plt.xlabel('False Negative')
+plt.ylabel('False Positive')
+
+plt.show()
